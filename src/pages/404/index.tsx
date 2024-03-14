94b62038272d02a97641ada21b9a5ec76f527014
Kartik Saxena <karrtiksaxena@gmail.com>
2024-03-15 03:41:15 +0530

    tailwind added

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import NotFoundImage from './404-error.png'; // Import the image
+
 export function NotFoundPage() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src={NotFoundImage} alt="404" className='w-1\/2'/> {/* Use the imported image */}
+      <h1 className="text-4xl font-bold text-green-200">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-500">The page you are looking for does not exist.</p>
     </div>
   );
 }
